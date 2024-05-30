fe69035489d8d065bb1e8fd78616660101a06ebe
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-05-30 23:46:07 +0300

    1.3 — 1.4 refactor courses to an array

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,16 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const parts = [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content
-        part1={part1}
-        exercises1={exercises1}
-        part2={part2}
-        exercises2={exercises2}
-        part3={part3}
-        exercises3={exercises3}
-      />
-      <Total
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
@@ -39,19 +27,17 @@ function Part({ name, numExercises }) {
   )
 }
 
-function Content(props) {
-  const { part1, exercises1, part2, exercises2, part3, exercises3 } = props
-
+function Content({ parts }) {
   return (
     <>
-      <Part name={part1} numExercises={exercises1} />
-      <Part name={part2} numExercises={exercises2} />
-      <Part name={part3} numExercises={exercises3} />
+      <Part name={parts[0].name} numExercises={parts[0].exercises} />
+      <Part name={parts[1].name} numExercises={parts[1].exercises} />
+      <Part name={parts[2].name} numExercises={parts[2].exercises} />
     </>
   )
 }
-function Total({ exercises1, exercises2, exercises3 }) {
-  return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+function Total({ parts }) {
+  return <p>Number of exercises {parts[0] + parts[1] + parts[2]}</p>
 }
 
 export default App
