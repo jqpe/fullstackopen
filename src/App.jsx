7aafbda4996e61d385314000ccb4fde604a1ef69
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-05-31 00:16:15 +0300

    1.6 complete functionality
    note to self: 1h 12 min used in part 1 at this point

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,55 @@
 import { useState } from 'react'
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  return <div>code here</div>
+  const data = [
+    { title: 'good', value: good },
+    { title: 'neutral', value: neutral },
+    { title: 'bad', value: bad }
+  ]
+
+  return (
+    <div>
+      <Header title="Give feedback" />
+      <Button handleClick={() => setGood(good => (good += 1))}>good</Button>
+      <Button handleClick={() => setNeutral(good => (good += 1))}>
+        neutral
+      </Button>
+      <Button handleClick={() => setBad(good => (good += 1))}>bad</Button>
+
+      <Header level="h2" title="Statistics" />
+      <Statistics data={data} />
+    </div>
+  )
+}
+
+function StatisticsRow({ title, value }) {
+  return (
+    <p>
+      {title} {value}
+    </p>
+  )
+}
+
+function Statistics({ data }) {
+  return (
+    <>
+      <StatisticsRow title={data[0].title} value={data[0].value} />
+      <StatisticsRow title={data[1].title} value={data[1].value} />
+      <StatisticsRow title={data[2].title} value={data[2].value} />
+    </>
+  )
+}
+
+function Button({ handleClick, children }) {
+  return <button onClick={handleClick}>{children}</button>
+}
+
+function Header({ title, level: Level = 'h1' }) {
+  return <Level>{title}</Level>
 }
 
 export default App
