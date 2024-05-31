af5ed3a4eed9d70076c5cbff9440ede5b0b565f3
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-05-31 14:36:57 +0300

    2.2 sum of exercises

--- src/App.jsx
+++ src/App.jsx
@@ -29,8 +29,15 @@ const Course = ({ course }) => {
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
+}
+
+function Total({ parts }) {
+  const total = parts.reduce((acc, part) => acc + part.exercises, 0)
+
+  return <strong>total of {total} exercises</strong>
 }
 
 function Header({ course }) {
