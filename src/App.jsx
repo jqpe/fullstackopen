bce66408a331d380c358f43a61f99569d6017c60
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-05-31 14:43:41 +0300

    2.5 separate module for course
    refactor components to use function declearation instead of arrow function for consistency.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-const App = () => {
+import { Course } from './Course'
+
+export default function App() {
   const courses = [
     {
       name: 'Half Stack application development',
@@ -52,39 +54,3 @@ const App = () => {
     </div>
   )
 }
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
-
-function Total({ parts }) {
-  const total = parts.reduce((acc, part) => acc + part.exercises, 0)
-
-  return <strong>total of {total} exercises</strong>
-}
-
-function Header({ course }) {
-  return <h1>{course}</h1>
-}
-
-function Part({ name, numExercises }) {
-  return (
-    <p>
-      {name} {numExercises}
-    </p>
-  )
-}
-
-function Content({ parts }) {
-  return parts.map(part => (
-    <Part key={part.id} name={part.name} numExercises={part.exercises} />
-  ))
-}
-
-export default App
