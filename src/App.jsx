c11e9341704d4f66631172923880c1472abc4fc3
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-05-31 15:38:28 +0300

    2.10 refactor into components
    - filter component
    - person form component
    - persons component
    - person component
    
    also move state down into person form as it's not used above

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,7 @@ const App = () => {
     { name: 'Arto Hellas' },
     { name: 'Minna Mansikka' }
   ])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
+
   const [searchQuery, setSearchQuery] = useState('')
 
   const shownPersons = searchQuery
@@ -18,44 +17,78 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <article>
-        Filter shown with{' '}
-        <input onChange={e => setSearchQuery(e.currentTarget.value)} />
-      </article>
+      <Filter handleChange={e => setSearchQuery(e.currentTarget.value)} />
 
       <h2>add new</h2>
-      <form
-        onSubmit={e => {
-          e.preventDefault()
-
-          if (persons.some(({ name }) => name === newName)) {
-            alert(`${newName} is already added to phonebook`)
-            return
-          }
-
-          setPersons([...persons, { name: newName, number: newNumber }])
-        }}
-      >
-        <div>
-          <div>
-            name: <input onChange={e => setNewName(e.currentTarget.value)} />
-          </div>
-          <div>
-            number:{' '}
-            <input onChange={e => setNewNumber(e.currentTarget.value)} />
-          </div>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm
+        persons={persons}
+        handleSubmit={newPerson => setPersons([...persons, newPerson])}
+      />
       <h2>Numbers</h2>
-      {shownPersons.map(person => (
-        <p key={person.name}>
-          {person.name} {person.number}
-        </p>
-      ))}
+      <Persons persons={shownPersons} />
     </div>
+  )
+}
+
+function PersonForm({ persons, handleSubmit }) {
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  return (
+    <form
+      // Too much abstraction?
+      onSubmit={e => {
+        e.preventDefault()
+
+        if (persons.some(({ name }) => name === newName)) {
+          alert(`${newName} is already added to phonebook`)
+          return
+        }
+
+        handleSubmit({ name: newName, number: newNumber })
+      }}
+    >
+      <div>
+        <div>
+          name: <input onChange={e => setNewName(e.currentTarget.value)} />
+        </div>
+        <div>
+          number: <input onChange={e => setNewNumber(e.currentTarget.value)} />
+        </div>
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+function Filter({ handleChange }) {
+  return (
+    <article>
+      Filter shown with <input onChange={handleChange} />
+    </article>
+  )
+}
+
+function Persons({ persons }) {
+  return (
+    <table>
+      <tbody>
+        {persons.map(person => (
+          <Person key={person.name} person={person} />
+        ))}
+      </tbody>
+    </table>
+  )
+}
+
+function Person({ person }) {
+  return (
+    <tr>
+      <td>{person.name}</td>
+      <td>{person.number}</td>
+    </tr>
   )
 }
 
