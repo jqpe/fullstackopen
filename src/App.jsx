ceae02b26e1f7b5822b39f7a3d5dc523f6281f8b
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-05-31 15:04:12 +0300

    2.6 add name

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import { useState } from 'react'
 
+// TODO: phone number so it's unique(ish)
+const getUniqueId = person => {
+  return person.name
+}
+
 const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
   const [newName, setNewName] = useState('')
@@ -7,16 +12,23 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form
+        onSubmit={e => {
+          e.preventDefault()
+          setPersons([...persons, { name: newName }])
+        }}
+      >
         <div>
-          name: <input />
+          name: <input onChange={e => setNewName(e.currentTarget.value)} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person => (
+        <p key={getUniqueId(person)}>{person.name}</p>
+      ))}
     </div>
   )
 }
