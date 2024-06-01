07a4686481aefa8505e601dc39ee0060046d4a10
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-06-01 09:24:50 +0300

    Squashed commit of part-2/phonebook:
    commit cd792be5597514b1436d2f43d28f6b84aaf7e95f
    Author: Jasper Nykänen <jasperjnykanen@gmail.com>
    Date:   Fri May 31 17:21:41 2024 +0300
    
        2.16 & 2.17 success and error states
    
    commit dca8a90f9794d61f247b119281a1cf9900a8749f
    Author: Jasper Nykänen <jasperjnykanen@gmail.com>
    Date:   Fri May 31 16:43:14 2024 +0300
    
        2.15 update person
    
    commit a2f64c79ed80ca9d7898460857449f1af39cbd4d
    Author: Jasper Nykänen <jasperjnykanen@gmail.com>
    Date:   Fri May 31 16:21:51 2024 +0300
    
        2.14 handle delete
    
    commit 6b184a9695f056a6effc63e4fc4a4368e615bb71
    Author: Jasper Nykänen <jasperjnykanen@gmail.com>
    Date:   Fri May 31 16:17:39 2024 +0300
    
        fix ids should be strings
    
        DELETE /persons/1 would not work if the id  was a number (got 404 not found)
    
    commit 0adeb0d6f79ef91333b2b3ba048509d986f4e93c
    Author: Jasper Nykänen <jasperjnykanen@gmail.com>
    Date:   Fri May 31 16:00:56 2024 +0300
    
        2.13 person service
    
    commit b0ebe2addb8f14a244723296496731c1066333fb
    Author: Jasper Nykänen <jasperjnykanen@gmail.com>
    Date:   Fri May 31 15:56:33 2024 +0300
    
        2.12 save numbers to server
    
    commit 464c97645c32f814f690203451f0bafe319cc259
    Author: Jasper Nykänen <jasperjnykanen@gmail.com>
    Date:   Fri May 31 15:47:00 2024 +0300
    
        2.11 fetch initial data from json-server
    
    commit c11e9341704d4f66631172923880c1472abc4fc3
    Author: Jasper Nykänen <jasperjnykanen@gmail.com>
    Date:   Fri May 31 15:38:28 2024 +0300
    
        2.10 refactor into components
    
        - filter component
        - person form component
        - persons component
        - person component
    
        also move state down into person form as it's not used above
    
    commit c69428ad3f38aff457bbafffcdc568486c84db01
    Author: Jasper Nykänen <jasperjnykanen@gmail.com>
    Date:   Fri May 31 15:27:30 2024 +0300
    
        2.9 filter shown people
    
    commit 0a7027ab3ee3a733acc312152a4ed87e00ed4677
    Author: Jasper Nykänen <jasperjnykanen@gmail.com>
    Date:   Fri May 31 15:12:50 2024 +0300
    
        2.8 allow specifying number
    
    commit 78b3f1765f993458bf75d92413a7f60a28b7a071
    Author: Jasper Nykänen <jasperjnykanen@gmail.com>
    Date:   Fri May 31 15:08:06 2024 +0300
    
        2.7 prevent duplicate name entries
    
        also remove the helper function since it's kinda moot
    
    commit ceae02b26e1f7b5822b39f7a3d5dc523f6281f8b
    Author: Jasper Nykänen <jasperjnykanen@gmail.com>
    Date:   Fri May 31 15:04:12 2024 +0300
    
        2.6 add name
    
    commit 4d0123d47878e21d058a8cb378a587828881f87f
    Author: Jasper Nykänen <jasperjnykanen@gmail.com>
    Date:   Fri May 31 14:46:46 2024 +0300
    
        2.6 boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,167 @@
+import { useEffect, useState } from 'react'
+import personService from './services/person'
+
+import './App.css'
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [searchQuery, setSearchQuery] = useState('')
+  const [notification, setNotification] = useState(null)
+
+  const showNotification = (message, variant) => {
+    setNotification({ message, variant: variant ?? 'success' })
+
+    setTimeout(() => {
+      setNotification(null)
+    }, 3000)
+  }
+
+  useEffect(() => {
+    personService.getAll().then(res => setPersons(res.data))
+  }, [])
+
+  const shownPersons = searchQuery
+    ? persons.filter(({ name }) => {
+        return new RegExp(searchQuery, 'gi').test(name)
+      })
+    : persons
+
+  const onSubmit = person => {
+    const phonebookPerson = persons.find(({ name }) => name === person.name)
+
+    if (
+      phonebookPerson &&
+      window.confirm(
+        `${person.name} is already added to phonebook, replace the old number with a new one?`
+      )
+    ) {
+      const updatedPerson = { ...phonebookPerson, ...person }
+      personService
+        .update(updatedPerson)
+        .then(() => {
+          const copy = [...persons]
+          const index = copy.findIndex(({ id }) => id === updatedPerson.id)
+
+          copy[index] = updatedPerson
+
+          setPersons(copy)
+        })
+        .then(showNotification(`Updated ${person.name}`))
+        .catch(() => {
+          showNotification(
+            `Can not update. Information of ${person.name} has been removed from the server`,
+            'error'
+          )
+        })
+      return
+    }
+
+    personService
+      .create(person)
+      .then(res => setPersons(persons.concat(res.data)))
+      .then(() => showNotification(`Added ${person.name}`))
+  }
+
+  const handleDelete = person => {
+    if (window.confirm(`Delete ${person.name}?`)) {
+      personService
+        .remove(person.id)
+        .then(res => setPersons(persons.filter(({ id }) => id !== res.data.id)))
+        .catch(() => {
+          showNotification(
+            `Information of ${person.name} has already been removed from the server`,
+            'error'
+          )
+        })
+    }
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Notification
+        message={notification?.message ?? null}
+        variant={notification?.variant}
+      />
+      <Filter handleChange={e => setSearchQuery(e.currentTarget.value)} />
+
+      <h2>add new</h2>
+      <PersonForm handleSubmit={onSubmit} />
+      <h2>Numbers</h2>
+      <Persons persons={shownPersons} handleDelete={handleDelete} />
+    </div>
+  )
+}
+
+function Notification({ message, variant }) {
+  if (message === null) {
+    return null
+  }
+
+  return <div className={variant}>{message}</div>
+}
+
+function PersonForm({ handleSubmit }) {
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  return (
+    <form
+      onSubmit={e => {
+        e.preventDefault()
+
+        handleSubmit({ name: newName, number: newNumber })
+      }}
+    >
+      <div>
+        <div>
+          name: <input onChange={e => setNewName(e.currentTarget.value)} />
+        </div>
+        <div>
+          number: <input onChange={e => setNewNumber(e.currentTarget.value)} />
+        </div>
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+function Filter({ handleChange }) {
+  return (
+    <article>
+      Filter shown with <input onChange={handleChange} />
+    </article>
+  )
+}
+
+function Persons({ persons, handleDelete }) {
+  return (
+    <table>
+      <tbody>
+        {persons.map(person => (
+          <Person
+            key={person.name}
+            person={person}
+            handleDelete={handleDelete}
+          />
+        ))}
+      </tbody>
+    </table>
+  )
+}
+
+function Person({ person, handleDelete }) {
+  return (
+    <tr>
+      <td>{person.name}</td>
+      <td>{person.number}</td>
+      <td>
+        <button onClick={() => handleDelete(person)}>delete</button>
+      </td>
+    </tr>
+  )
+}
+
+export default App
---
+++ src/services/person.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+const API_ENDPOINT = 'http://localhost:3001/persons'
+
+export const getAll = () => {
+  return axios.get(API_ENDPOINT)
+}
+
+export const create = newObject => {
+  return axios.post(API_ENDPOINT, newObject)
+}
+
+export const remove = id => {
+  return axios.delete([API_ENDPOINT, id].join('/'))
+}
+
+export const update = person => {
+  return axios.put([API_ENDPOINT, person.id].join('/'), person)
+}
+
+const personService = { getAll, create, remove, update }
+
+export default personService
