6e8f806fc6d9e05293862d690fae5f361631a879
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-06-03 12:43:11 +0300

    4.2 logger module

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 
 const config = require('./utils/config.js')
-const blogs = require('./controllers/blogs.js')
+const blogsRouter = require('./controllers/blogs.js')
+const logger = require('./utils/logger.js')
 
 mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
-app.use(blogs)
+app.use(blogsRouter)
 
 const PORT = config.PORT
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen(PORT, () => logger.info(`Server running on port ${PORT}`))
---
+++ utils/logger.js
@@ -0,0 +1,5 @@
+module.exports = {
+  info(...args) {
+    console.log(...args)
+  }
+}
