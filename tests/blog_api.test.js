6f0f51d7fcae5faea4f8dd75158c4935b592d66f
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-06-03 14:44:42 +0300

    4.10 test post request

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -57,6 +57,25 @@ test('_id is available under id key', async () => {
   assert.strictEqual(response.body[0].id, ID.toString())
 })
 
+test('submitting works', async () => {
+  const newBlog = { ...initialBlogs[1], title: 'another one!' }
+
+  const response = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const body = { ...response.body }
+  // another way would be to send the id with the request
+  delete body.id
+
+  assert.deepEqual(body, newBlog)
+
+  const blogs = await Blog.find({})
+  assert.strictEqual(blogs.length, 3)
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
