d0303adbcf743d4d593a7b08e5f8792704a24ac5
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-06-03 23:21:21 +0300

    5.7 expandable blog

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,21 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <details open={open} onClick={e => e.preventDefault()}>
+      <summary>
+        {blog.title} {blog.author}{' '}
+        <button onClick={() => setOpen(!open)}>{open ? 'hide' : 'view'}</button>
+      </summary>
+      <a href={blog.url}>{blog.url}</a>
+      <div>
+        likes {blog.likes} <button>like</button>
+      </div>
+      <p>{blog.author}</p>
+    </details>
+  )
+}
 
 export default Blog
