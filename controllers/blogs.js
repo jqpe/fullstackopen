3b937d66e9fe7da1e53675aae7b2bfc1a92d53c1
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-06-03 14:26:13 +0300

    4.8 refactor to async/await

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,14 +3,16 @@ const { Blog } = require('../models/blog.js')
 
 const blogs = express.Router()
 
-blogs.get('/api/blogs', (_, res) => {
-  Blog.find({}).then(blogs => res.json(blogs))
+blogs.get('/api/blogs', async (_, res) => {
+  const blogs = await Blog.find({})
+
+  res.json(blogs)
 })
 
-blogs.post('/api/blogs', (req, res) => {
-  const blog = new Blog(req.body)
+blogs.post('/api/blogs', async (req, res) => {
+  const blog = await new Blog(req.body).save()
 
-  blog.save().then(result => res.status(201).json(result))
+  res.status(201).json(blog)
 })
 
 module.exports = blogs
