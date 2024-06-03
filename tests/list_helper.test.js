a992794ab3886fe6d3ea15d7ecddef249cd7d179
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-06-03 13:14:18 +0300

    4.5 favorite blog

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,3 +1,7 @@
+// I've opted to use just the bare necessities for the mock data.
+// In practise object having extraneous properties should not change the logic at all.
+// Having properties not related to the expected functionality makes the tests harder to read and maintain.
+
 const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper.js')
@@ -26,9 +30,45 @@ describe('total likes', () => {
   test('of a bigger list is calculated right', () => {
     // sum of 0, 1, 2, ... 10
     const likes = listHelper.totalLikes(
-      Array.from({ length: 10 }).map((_, i) => ({ likes: i  }))
+      Array.from({ length: 10 }).map((_, i) => ({ likes: i }))
     )
 
     assert.strictEqual(likes, 45)
+  })
+})
+
+describe('favorite blog', () => {
+  test('returns the blog with most likes with weird input', () => {
+    const blogs = [{ likes: -2 }, { likes: 2 }]
+    const result = listHelper.favoriteBlog(blogs)
+
+    assert.deepStrictEqual(result, blogs[1])
+  })
+
+  test('returns the blog with most likes', () => {
+    const blogs = [{ likes: 0 }, { likes: 2 }]
+    const result = listHelper.favoriteBlog(blogs)
+
+    assert.deepStrictEqual(result, blogs[1])
+  })
+
+  test('may return undefined on an empty list', () => {
+    assert.strictEqual(listHelper.favoriteBlog([]), undefined)
+  })
+
+  test('returns first when there are multiple favorites', () => {
+    const blogs = [{ likes: 1 }, { likes: 200 }, { likes: 120 }, { likes: 200 }]
+    const result = listHelper.favoriteBlog(blogs)
+
+    assert.deepStrictEqual(result, blogs[1])
+  })
+
+  test('does not modify original array', () => {
+    const blogs = [{ likes: 1 }, { likes: 2 }]
+    const original = structuredClone(blogs)
+
+    listHelper.favoriteBlog(blogs)
+
+    assert.deepStrictEqual(blogs, original)
   })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,12 @@ const totalLikes = blogs => {
   return blogs.reduce((prev, curr) => prev + curr.likes, 0)
 }
 
+const favoriteBlog = blogs => {
+  return blogs.toSorted((a, b) => b.likes - a.likes).at(0)
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
