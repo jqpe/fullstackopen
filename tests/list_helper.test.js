a71eca706f8dcd3e556364e3d1d275bc623ae4eb
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-06-03 12:59:50 +0300

    4.4 add total likes

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,4 +1,4 @@
-const { test } = require('node:test')
+const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper.js')
 
@@ -7,4 +7,28 @@ test('dummy returns one', () => {
 
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
+})
+
+describe('total likes', () => {
+  test('of empty list is zero', () => {
+    const likes = listHelper.totalLikes([])
+
+    assert.strictEqual(likes, 0)
+  })
+
+  test('when list has only one blog equals the likes of that', () => {
+    const random = Math.floor(Math.random() * 100)
+    const likes = listHelper.totalLikes([{ likes: random }])
+
+    assert.strictEqual(likes, random)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    // sum of 0, 1, 2, ... 10
+    const likes = listHelper.totalLikes(
+      Array.from({ length: 10 }).map((_, i) => ({ likes: i  }))
+    )
+
+    assert.strictEqual(likes, 45)
+  })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,11 @@ const dummy = _blogs => {
   return 1
 }
 
+const totalLikes = blogs => {
+  return blogs.reduce((prev, curr) => prev + curr.likes, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
