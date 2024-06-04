79ee94528e089eabafd7098e4f1d093bf89cabb0
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-06-04 11:19:34 +0300

    5.12 add eslint-plugin-unicorn
    https://github.com/sindresorhus/eslint-plugin-unicorn

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,13 +5,18 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended'
+    'plugin:react-hooks/recommended',
+    'plugin:unicorn/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    // in React world null is actually very useful
+    "unicorn/no-null": 0,
+    // We could configure this with overloads but ehh
+    "unicorn/filename-case": 0,
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true }
