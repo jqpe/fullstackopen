535577f5bd51eabbffe2797ac89c668f547ce073
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-06-04 15:23:05 +0300

    fix: send populated user

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,9 +1,7 @@
 const express = require('express')
-const jwt = require('jsonwebtoken')
 
 const { Blog } = require('../models/blog.js')
 const { User } = require('../models/user.js')
-const config = require('../utils/config.js')
 
 const blogs = express.Router()
 
@@ -21,14 +19,15 @@ blogs.post('/', async (req, res) => {
   if (!req.user) {
     return res.status(401).json({ error: 'token invalid' })
   }
-  
+
   const user = await User.findById(req.user)
 
   const blog = await new Blog({ ...req.body, user: user.id }).save()
+  const withUser = await blog.populate('user')
   user.blogs = user.blogs.concat(blog._id)
   await user.save()
 
-  res.status(201).json(blog)
+  res.status(201).json(withUser)
 })
 
 blogs.delete('/:id', async (req, res) => {
@@ -52,8 +51,9 @@ blogs.put('/:id', async (req, res) => {
     // ensures we return the updated blog, by default mongoose returns the altered one
     new: true
   })
+  const withUser = await blog.populate("user")
 
-  res.json(blog)
+  res.json(withUser)
 })
 
 module.exports = blogs
