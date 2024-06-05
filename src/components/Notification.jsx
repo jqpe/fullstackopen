b5406fbbc6617e59695960ff2f40dea961cd930d
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-06-05 15:11:55 +0300

    fix notification component

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,22 +1,15 @@
-import { useState } from 'react'
-import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 
 export function Notification() {
   const notification = useSelector((state) => state.notification)
 
-  const [className, setClassname] = useState(notification.variant)
-
-  useEffect(() => {
-    if (!notification.message) return
-    setClassname(notification.variant)
-
-    setTimeout(() => setClassname(undefined), 3000)
-  }, [notification])
-
-  if (!notification.message || !className) {
+  if (!notification.message) {
     return null
   }
 
-  return <aside className={className}>{notification.message}</aside>
+  return (
+    <aside className={`notification ${notification.variant}`}>
+      {notification.message}
+    </aside>
+  )
 }
