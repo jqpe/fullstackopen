9a48f58e0d97d3e7e3b55559aa05196fa186e2a3
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-06-06 16:48:47 +0300

    8.9 books

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,5 +1,18 @@
+import { gql, useQuery } from '@apollo/client'
+
+const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      title
+      author
+      published
+    }
+  }
+`
+
 const Books = () => {
-  const books = []
+  const result = useQuery(ALL_BOOKS)
+  const books = result.data?.allBooks ?? []
 
   return (
     <div>
